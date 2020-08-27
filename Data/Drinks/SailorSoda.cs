cea82501d9eeb2b9c40cbfde6286e841883c9655
Laura Legg <laura.art98@gmail.com>
2020-08-26 21:23:04 -0500

    Milestone 1 - Started work on classes in Drinks folder

---
+++ Data/Drinks/SailorSoda.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Author: Laura Legg
+ * Class name: SailorSoda.cs
+ * Purpose: Class used to represent the Sailor Soda.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BleakwindBuffet.Data.Drinks
+{
+    class SailorSoda
+    {
+        public double Price => ;
+
+        public uint Calories => ;
+
+        public List<string> SpecialInstructions
+        {
+            get
+            {
+                List<string> instructions = new List<string>();
+                return instructions;
+            }
+        }
+
+        public bool Ice { get; set; } = true;
+    }
+}
